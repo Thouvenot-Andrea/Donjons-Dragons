eb5e5aa21a07efd6e011a4de1be55ea2316c99b4
thouvenot <thouvenot@thouvenot-desktop>
2024-04-29 13:37:25 +0200

    feat : class Personnage

--- src/jeu/personnages/Personnage.java
+++ src/jeu/personnages/Personnage.java
@@ -1,36 +1,101 @@
 package jeu.personnages;
 
 public class Personnage {
-        //attributes
+    //attributes
     private String name;
+    private int pv;
+    private int damage;
     private String type;
-        //constructor
-    public Personnage(){
+    private String equipementOffensif;
+    private String equipementDefensif;
+
+    //constructor
+    public Personnage() {
 
     }
-    public Personnage(String name){
-        this.name=name;
+
+    public Personnage(String name) {
+        this.name = name;
     }
-    public Personnage(String name, String type){
-        this.name =name;
-        this.type=type;
+
+    public Personnage(String name, int pv, int damage) {
+        this.name = name;
+        this.pv = pv;
+        this.damage = damage;
+
     }
-        //getter
-    public String getName(){
+
+
+    //name ( Getter et Setter)
+    public String getName() {
         return name;
     }
-    public String getType(){
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    // pv ( Getter et Setter)
+    public int getPv() {
+        return pv;
+    }
+
+    public void setPv(int pv) {
+        this.pv = pv;
+    }
+
+
+    //damage ( Getter et Setter)
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+
+    //type ( Getter et Setter)
+    public String getType() {
         return type;
     }
-        //setter
-    public void setName(String name){
-        this.name=name;
+
+    public void setType(String type) {
+        this.type = type;
     }
-    public void setType(String type){
-        this.type=type;
+
+
+    // équipement offensif ( Getter et Setter)
+    public String getEquipementOffensif() {
+        return equipementOffensif;
     }
-        // toString
-    public String toString(){
-        return "Personnage [name=" + name + ", type=" + type + "]";
+
+    public void setEquipementOffensif(String equipementOffensif) {
+        this.equipementOffensif = equipementOffensif;
+    }
+
+
+    // équipement défensif ( Getter et Setter)
+    public String getEquipementDefensif() {
+        return equipementDefensif;
+    }
+
+    public void setEquipementDefensif(String equipementDefensif) {
+        this.equipementDefensif = equipementDefensif;
+    }
+
+
+    // toString
+    public String toString() {
+        return "\n" +
+                "   Type: " + type + "\n" +
+                "   Name: " + name + "\n" +
+                "   Damage: " + damage + "\n" +
+                "   Pv: " + pv + "\n" +
+                "   Equipement Offensif: " + equipementOffensif + "\n" +
+                "   Equipement Defensif: " + equipementDefensif +
+                "\n";
+
     }
 }
