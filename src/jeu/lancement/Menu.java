b8ed98348bf919f9c87d9c38b3c248711f1af388
thouvenot <thouvenot@thouvenot-desktop>
2024-04-30 11:05:53 +0200

    test

--- src/jeu/lancement/Menu.java
+++ src/jeu/lancement/Menu.java
@@ -60,6 +60,7 @@ public class Menu {
 
 
 
+
     // Méthode pour créer automatiquement un personnage par défaut
     private void createDefaultPerson() {
         System.out.println("Personnage créé automatiquement.");
