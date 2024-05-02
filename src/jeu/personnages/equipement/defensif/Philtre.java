c8caff3dffd494b75329394de73dc785bbd92d2c
thouvenot <thouvenot@thouvenot-desktop>
2024-05-02 14:27:04 +0200

    create class

--- src/jeu/personnages/equipement/defensif/Philtre.java
+++ src/jeu/personnages/equipement/defensif/Philtre.java
@@ -1,5 +1,8 @@
 package jeu.personnages.equipement.defensif;
 
 public class Philtre extends EquipementDefensif {
+    public Philtre(String type,int defenceLevel) {
+        super(type, defenceLevel);
+    }
 
 }
--- src/jeu/personnages/equipement/offensif/Sort.java
+++ src/jeu/personnages/equipement/offensif/Sort.java
@@ -1,6 +1,8 @@
 package jeu.personnages.equipement.offensif;
 
 public class Sort extends EquipementOffensif {
-
+    public Sort (String type,int attaqueLevel){
+        super(type,attaqueLevel);
+    }
 
 }
