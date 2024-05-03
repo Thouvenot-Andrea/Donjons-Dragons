062e914caca4e1808bb391f0716e9c09011af160
thouvenot <thouvenot@thouvenot-desktop>
2024-05-03 11:27:54 +0200

    feat : modification of the class, it just calls the class game

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
 import jeu.PersonnageHorsPlateauException;
 import jeu.lancement.Game;
-import jeu.lancement.Menu;
+
 
 public class Main {
     public static void main(String[] args) throws PersonnageHorsPlateauException {
-        Menu menu = new Menu();
 
-        while (menu.menuGame()) {
-            Game game = new Game(menu);
-            menu.display("Welcome to Donjons & Dragons!");
-            game.play();
-        }
+        Game game = new Game();
+        game.play();
+
     }
 }
 
