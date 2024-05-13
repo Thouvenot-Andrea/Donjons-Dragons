6f1827e40f8595e2c29c945e95b5970f88e1e7db
thouvenot <thouvenot@thouvenot-desktop>
2024-05-13 11:23:44 +0200

    feat :modification class warrior and mage

--- src/jeu/personnages/Mage.java
+++ src/jeu/personnages/Mage.java
@@ -1,14 +1,39 @@
 package jeu.personnages;
 
+import jeu.personnages.equipement.defensif.Philtre;
+import jeu.personnages.equipement.offensif.Sort;
+
 public class Mage extends Personnage {
 
+    private final Philtre equipementDefensif = new Philtre();
+    private final Sort  equipementOffensif =  new Sort();
+
     public Mage(String name) {
-        super(name,6,15);
+        super(name, 6 , 15);
         this.setType("MAGE");
-        this.setPv(6);
-        this.setDamage(15);
-        this.setEquipementDefensif("PHILTRE");
-        this.setEquipementOffensif("SORT");
+
+        Philtre philtre = new Philtre();
+        this.setEquipementDefensif(String.valueOf(philtre));
+        this.setPv(this.getPv() + philtre.getDefenceLevel());
+
+        Sort sort = new Sort();
+        this.setEquipementOffensif(String.valueOf(sort));
+        this.setDamage(this.getDamage() + sort.getAttaqueLevel());
+
+    }
+
+    @Override
+
+    public String toString() {
+        String parentString = super.toString();
+
+        String pvString = "Pv: " + getPv() + " (" + (getPv() - equipementDefensif.getDefenceLevel()) + "+" + equipementDefensif.getDefenceLevel() + ")";
+        parentString = parentString.replaceFirst("Pv: \\d+", pvString);
+
+        String damageString = "Damage: " + getDamage() + " (" + (getDamage() - equipementOffensif.getAttaqueLevel()) + "+" + equipementOffensif.getAttaqueLevel() + ")";
+        parentString = parentString.replaceFirst("Damage: \\d+", damageString);
+
+        return parentString;
     }
 
 }
