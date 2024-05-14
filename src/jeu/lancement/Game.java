8782f34e3b6cf3b50f4d56c1e7d8e596b2c3c8e0
thouvenot <thouvenot@thouvenot-desktop>
2024-05-14 14:47:33 +0200

    feat :modifiy for display and interagir case

--- src/jeu/lancement/Game.java
+++ src/jeu/lancement/Game.java
@@ -1,42 +1,89 @@
 package jeu.lancement;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.Random;
 
 import jeu.PersonnageHorsPlateauException;
 import jeu.personnages.Personnage;
+import jeu.plateau.Plateau;
+import jeu.plateau.casePlateau.Case;
+import jeu.plateau.casePlateau.ennemi.Ennemi;
 import jeu.plateau.casePlateau.vide.CaseVide;
-import jeu.plateau.casePlateau.potion.Potion;
 import jeu.plateau.dice.DiceOne;
-import jeu.plateau.casePlateau.potion.CasePotion;
 import jeu.plateau.casePlateau.ennemi.Dragons;
 
 
 public class Game {
-    Menu menu = new Menu();
     private final Random number = new Random();
-    private Personnage personnage;
 
-    public void play() throws PersonnageHorsPlateauException {
-        menu.display("Bienvenue dans Donjons & Dragons !");
-        menu.menuGame();
+    private final Personnage personnage;
+    private final Menu menu;
+    private final Plateau plateau;
+    private final DiceOne dice;
+
+    public Game(Plateau plateau, Personnage personnage, Menu menu) {
+        this.plateau = plateau;
+        this.personnage = personnage;
+        this.menu = menu;
+        dice = new DiceOne();
     }
 
-//    private final Random random = new Random();
+    public void play() {
+        int currentPosition = 1;
+        int finalPosition = 64;
+        boolean playerWon = false;
+        try {
+            while (currentPosition < finalPosition) {
+                currentPosition = movePersonnage(currentPosition);
 
-    private Set<Integer> initialiserCasesDragons() {
-        Set<Integer> casesDragonsSet = new HashSet<>();
-        // Ajouter les positions des dragons au set
-        casesDragonsSet.add(45);
-        casesDragonsSet.add(52);
-        casesDragonsSet.add(56);
-        casesDragonsSet.add(62);
-        return casesDragonsSet;
+                Case gameCase = plateau.getCase(currentPosition);
+
+                menu.displacementPlayer(currentPosition, finalPosition);
+                menu.heroLandsOnCase(gameCase);
+
+                if (gameCase instanceof Ennemi) {
+                    Ennemi ennemi = (Ennemi) gameCase;
+                    String combatResult = ennemi.interagir(personnage);
+                    menu.showCombatResult(combatResult); // Afficher les informations du combat
+                    if (personnage.getPv() <= 0) {
+                        menu.defeat("Vous avez été vaincu. Game Over !");
+                        return; // Laisser la logique de redémarrage au main
+                    }
+                } else {
+                    gameCase.interagir(personnage);
+                }
+
+                if (personnage.getPv() <= 0) {
+                    menu.defeat("Vous avez été vaincu. Game Over !");
+                    return;
+                }
+
+            }
+            playerWon = true;
+        } catch (PersonnageHorsPlateauException e) {
+            menu.exception(e.getMessage());
+        }
+        if (playerWon) {
+            menu.end("Félicitations ! Vous avez GAGNÉ !!!!!!!!!!");
+        }
+
     }
 
+    private int movePersonnage(int currentPosition) throws PersonnageHorsPlateauException {
+        int diceRoll = dice.lancer();
+        menu.de(diceRoll);
+        int newPosition = currentPosition + diceRoll;
 
-    //CaseVide
+        if (currentPosition >= plateau.size() - 1) {
+            throw new PersonnageHorsPlateauException();
+        }
+
+        return newPosition;
+    }
+
+}
+
+
+//CaseVide
 //    private Set<Integer> initialiserCasesVides() {
 //        Set<Integer> casesVidesSet = new HashSet<>();
 //        Random random = new Random();
@@ -56,50 +103,38 @@ public class Game {
 //        }
 //        return casesPotionSet;
 //    }
-    private void combattreDragon(Personnage personnage, Dragons dragon) throws PersonnageHorsPlateauException {
-        // Afficher le message de rencontre avec le dragon
-        menu.dragonEncounter("Vous êtes tombé sur un dragon ! Préparez-vous au combat !");
+//    private void combattreDragon(Personnage personnage, Dragons dragon) throws PersonnageHorsPlateauException {
+//        // Afficher le message de rencontre avec le dragon
+//        menu.dragonEncounter("Vous êtes tombé sur un dragon ! Préparez-vous au combat !");
+//
+//        // Boucle de combat jusqu'à ce que le joueur ou le dragon soit vaincu
+//        while (personnage.getPv() > 0 && dragon.getHealth() > 0) {
+//            // Attaque du joueur sur le dragon
+//            int degatsInfligesParLeJoueur = personnage.getDamage();
+//            dragon.takeDamage(degatsInfligesParLeJoueur);
+//
+//            // Vérifier si le dragon est vaincu
+//            if (dragon.getHealth() <= 0) {
+//                menu.end("Vous avez vaincu le dragon ! Félicitations !");
+//                return; // Sortir de la méthode après avoir vaincu le dragon
+//            }
+//
+//            // Attaque du dragon sur le joueur
+//            int degatsInfligesParLeDragon = 4; // Le dragon inflige 4 dégâts au joueur
+//            personnage.setPv(personnage.getPv() - degatsInfligesParLeDragon);
+//
+//            // Vérifier si le joueur est vaincu
+//            if (personnage.getPv() <= 0) {
+//                throw new PersonnageHorsPlateauException(); // Lancer une exception si le joueur est vaincu
+//            }
+//        }
+//    }
 
-        // Boucle de combat jusqu'à ce que le joueur ou le dragon soit vaincu
-        while (personnage.getPv() > 0 && dragon.getHealth() > 0) {
-            // Attaque du joueur sur le dragon
-            int degatsInfligesParLeJoueur = personnage.getDamage();
-            dragon.takeDamage(degatsInfligesParLeJoueur);
-
-            // Vérifier si le dragon est vaincu
-            if (dragon.getHealth() <= 0) {
-                menu.end("Vous avez vaincu le dragon ! Félicitations !");
-                return; // Sortir de la méthode après avoir vaincu le dragon
-            }
-
-            // Attaque du dragon sur le joueur
-            int degatsInfligesParLeDragon = 4; // Le dragon inflige 4 dégâts au joueur
-            personnage.setPv(personnage.getPv() - degatsInfligesParLeDragon);
-
-            // Vérifier si le joueur est vaincu
-            if (personnage.getPv() <= 0) {
-                throw new PersonnageHorsPlateauException(); // Lancer une exception si le joueur est vaincu
-            }
-        }
-    }
-
-    public void movePlayer() throws PersonnageHorsPlateauException {
-        int currentPosition = 1;
-        int finalPosition = 64;
-        final DiceOne dice = new DiceOne();
-        final CaseVide caseVide = new CaseVide();
 
 //        Set<Integer> casesVides = initialiserCasesVides();
 //        Set<Integer> casesPotion = initialiserCasesPotion();
-        Set<Integer> casesDragons = initialiserCasesDragons(); // Ajout de l'initialisation des cases de dragons
+//        Set<Integer> casesDragons = initialiserCasesDragons(); // Ajout de l'initialisation des cases de dragons
 
-        try {
-            while (currentPosition < finalPosition) {
-                int diceRoll = dice.lancer(); // Lancer le dé à chaque tour
-                currentPosition += diceRoll;
-
-                menu.de("Vous avez lancé le Dé, résultat : " + diceRoll);
-                menu.displacementPlayer("Avancement du joueur : Case " + currentPosition + " / " + finalPosition);
 
 //                if (casesPotion.contains(currentPosition)) {
 //                    CasePotion casePotion = new CasePotion();
@@ -114,55 +149,22 @@ public class Game {
 //                    menu.de(message); // Afficher le résultat du dé
 //                }
 
-                if (casesDragons.contains(currentPosition)) {
-                    menu.dragonEncounter("Vous êtes tombé sur un dragon ! Préparez-vous au combat !");
-                    // Gestion du combat avec le dragon
-                    Dragons dragon = new Dragons(15); // Le dragon a 15 points de vie
-                    while (personnage.getPv() > 0 && dragon.getHealth() > 0) {
-                        // Combat entre le joueur et le dragon
-                        int playerDamageDealt = personnage.getDamage();
-                        dragon.takeDamage(playerDamageDealt);
-                        if (dragon.getHealth() > 0) {
-                            int dragonDamageDealt = 4; // Le dragon inflige 4 dégâts au joueur
-                            personnage.takeDamage(dragonDamageDealt);
-                        }
-                    }
-                    if (personnage.getPv() <= 0) {
-                        menu.defeat("Vous avez été vaincu par le dragon. Game Over !");
-                        replay(); // Proposer de rejouer après la défaite
-                    } else {
-                        menu.victory("Vous avez vaincu le dragon !");
-                    }
-                }
+//                if (casesDragons.contains(currentPosition)) {
+//                    menu.dragonEncounter("Vous êtes tombé sur un dragon ! Préparez-vous au combat !");
+//                    // Gestion du combat avec le dragon
+//                    Dragons dragon = new Dragons(15); // Le dragon a 15 points de vie
+//                    while (personnage.getPv() > 0 && dragon.getHealth() > 0) {
+//                        // Combat entre le joueur et le dragon
+//                        int playerDamageDealt = personnage.getDamage();
+//                        dragon.takeDamage(playerDamageDealt);
+//                        if (dragon.getHealth() > 0) {
+//                            int dragonDamageDealt = 4; // Le dragon inflige 4 dégâts au joueur
+//                            personnage.takeDamage(dragonDamageDealt);
+//                        }
+//                    }
+//                }
 
-                if (currentPosition > finalPosition) {
-                    throw new PersonnageHorsPlateauException();
-                }
-            }
-            menu.end("Félicitations ! Vous avez GAGNÉ !!!!!!!!!!");
-            replay(); // Proposer de rejouer après la victoire
-        } catch (PersonnageHorsPlateauException e) {
-            menu.exception(e.getMessage());
-            handleException();
-        }
-    }
+//    }
 
 
-
-    private void handleException() throws PersonnageHorsPlateauException {
-        replay();
-    }
-
-    private void replay() throws PersonnageHorsPlateauException {
-        menu.restart("Voulez-vous rejouer ? (yes or no)");
-        String response = menu.getUserInput();
-
-        if (response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y")) {
-            movePlayer(); // Appel récursif pour rejouer
-        } else {
-            menu.thanks("Merci d'avoir joué :)");
-        }
-    }
-
-
-}
+//}
