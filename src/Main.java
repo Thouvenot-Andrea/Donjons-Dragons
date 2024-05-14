239917eea15cb2c168593119849179a181721579
thouvenot <thouvenot@thouvenot-desktop>
2024-05-14 14:44:33 +0200

    feat :adding of main the function to launch the menu and replay the game

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,34 @@
 import jeu.PersonnageHorsPlateauException;
 import jeu.lancement.Game;
+import jeu.lancement.Menu;
+import jeu.personnages.Personnage;
+import jeu.plateau.Plateau;
 
 
 public class Main {
     public static void main(String[] args) throws PersonnageHorsPlateauException {
 
-        Game game = new Game();
-        game.play();
+        Menu menu = new Menu();
+        menu.display("Bienvenue dans Donjons & Dragons !");
+
+        boolean replay = true;
+        while (replay) {
+
+            Plateau plateau = new Plateau();
+            Personnage personnage = menu.menuGame();
+            Game game = new Game(plateau, personnage, menu);
+            game.play();
+
+            menu.restart("Voulez-vous rejouer ? (yes or no)");
+            String response = menu.getUserInput();
+
+            if (response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y")) {
+                replay = true;
+            } else {
+                replay = false;
+                menu.thanks("Merci d'avoir joué :)");
+            }
+        }
 
     }
 }
