cf94f3f851d22539160fc38f2b2ae1f7ee37138d
thouvenot <thouvenot@thouvenot-desktop>
2024-05-14 16:38:11 +0200

    feat :create class

--- src/jeu/plateau/casePlateau/ennemi/Gobelins.java
+++ src/jeu/plateau/casePlateau/ennemi/Gobelins.java
@@ -1,34 +1,12 @@
 package jeu.plateau.casePlateau.ennemi;
 
-import jeu.personnages.Personnage;
-
-public class Gobelins implements CaseEnnemi {
-    private int health;
-
+public class Gobelins extends Ennemi {
     public Gobelins(int health) {
-        this.health = health;
-    }
-
-    // Méthode pour infliger des dégâts au dragon
-    public void takeDamage(int damage) {
-        health -= damage;
-        if (health < 0) {
-            health = 0; // Assurez-vous que la santé du dragon ne devienne pas négative
-        }
-    }
-
-    // Méthode pour récupérer les points de vie actuels du dragon
-    public int getHealth() {
-        return health;
+        super(health);
     }
 
     @Override
     public String afficher() {
-        return "Gobelins"; //
-    }
-
-    @Override
-    public String interagir(Personnage hero) {
-        return "";
+        return "Gobelin";
     }
 }
