df708ef7ec5366becd378077ebdb72a2a308bfa1
thouvenot <thouvenot@thouvenot-desktop>
2024-05-21 09:24:19 +0200

    feat :modify code in the Epee class

--- src/jeu/plateau/casePlateau/arme/Epee.java
+++ src/jeu/plateau/casePlateau/arme/Epee.java
@@ -3,30 +3,47 @@ package jeu.plateau.casePlateau.arme;
 
 import jeu.lancement.Menu;
 import jeu.personnages.Personnage;
+import jeu.personnages.Warrior;
 
 public class Epee extends Arme {
 
-    public Epee(int armeDamage){
-        super(armeDamage);
+    public Epee(int attaquePv){
+        super(attaquePv);
     }
-
-    @Override
-    public int ArmeDamage() {
-        return 5;
-    }
-
     @Override
     public String afficher() {
-        return "une Epee";
-    }
-
-    @Override
-    public String interagir(Personnage personnage) {
-        return "La Epee interagit avec le personnage.";
+        return "EPEE";
     }
 
     @Override
     public void interagirAvecJoueur(Personnage personnage, Menu menu) {
-
+        String armeResult = interagir(personnage);
+        menu.showCombatResult(armeResult);
     }
+
+
+    @Override
+    public String interagir(Personnage personnage) {
+        StringBuilder armeResult = new StringBuilder();
+        if (personnage instanceof Warrior warrior) {
+            int currentWeaponDamage = warrior.attaqueArme();
+            if (currentWeaponDamage < this.getArmeDamage()) {
+                armeResult.append("L'arme précédente est remplacée.\n");
+                int totalDamage = warrior.getDamageBase() + this.getArmeDamage();
+                warrior.setAddArme(this.afficher(),this.getArmeDamage());
+                warrior.setDamage(totalDamage);
+                warrior.setEquipementOffensif(String.valueOf(this));
+                armeResult.append("Vous vous équiper d'une ").append(this.afficher()).append("\n");
+                armeResult.append("Vous infligez ").append(this.getArmeDamage()).append(" points de dégâts avec cette arme en plus des dégats de base.\n");
+            } else {
+                armeResult.append("L'arme n'est pas plus puissante que l'arme actuelle.\n");
+                armeResult.append("Vous conservez votre arme.\n");
+            }
+        } else {
+            armeResult.append("Seuls les Warriors peuvent équiper cette Arme!!!!");
+        }
+        return armeResult.toString();
+    }
+
+
 }
